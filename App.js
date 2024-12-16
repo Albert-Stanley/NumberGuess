c434efa2c198bb0bc7c020d33f50b8e403c345ea
Albert Stanley <153841479+Albert-Stanley@users.noreply.github.com>
2024-12-16 15:21:26 -0300

    Adding a linear gradient by expo dependency.

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient
+      colors={["#4e0329", "#ddb52f"]}
+      style={styles.rootContainer}
+    >
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+});
